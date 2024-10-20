72dd69c7a5ef24ddd1cedb94fc3658996380b7af
PatrickJurt <patrick.jurt@gmx.ch>
2024-10-20 14:58:19 +0200

    reading timers from storage and finishing when ready

--- grepo-automation/AutoFinishBuilding.js
+++ grepo-automation/AutoFinishBuilding.js
@@ -1,7 +1,5 @@
 (function () {
 
-    
-
     function awaitLoading(){
         return new Promise((resolve) => {
             const checkExistence = setInterval(() => {
@@ -9,7 +7,6 @@
                 if (loader){
                     if (loader.style.visibility === 'hidden'){
                         clearInterval(checkExistence);
-                        console.log('Awaited Loading')
                         resolve();
                     }
                 } else{
@@ -18,6 +15,7 @@
             }, 100);
         });
     }
+
 
     function getCityName(){
         const town = document.querySelector('.town_name');
@@ -35,13 +33,12 @@
         calculatedTime.setMinutes(currentTime.getMinutes() + minutes);
         calculatedTime.setSeconds(currentTime.getSeconds() + seconds);
         calculatedTime.setMinutes(calculatedTime.getMinutes() - 5);
-        calculatedTime.setSeconds(calculatedTime.getSeconds() - 5);
+        calculatedTime.setSeconds(calculatedTime.getSeconds() + 5);
 
         return calculatedTime.toISOString();
     }
 
     function addBuildingIntoAutomationQueue(restTime){
-        console.log('addBuildingIntoAutomationQueue', restTime);
         try{
             let buildingFinishTimer = localStorage.getItem('buildingFinishTimer');
 
@@ -62,10 +59,41 @@
             const newEntry = { cityName: getCityName(), timestamp: getTimestamp(restTime) };
             buildingFinishTimer.push(newEntry);
             localStorage.setItem('buildingFinishTimer', JSON.stringify(buildingFinishTimer));
-            console.log('Updated buildingFinishTimer in local Storage:', buildingFinishTimer);
 
         } catch (e) {
             console.error("Error updating 'buildingFinishTimer':", e);
+        }
+    }
+
+    function checkQueueItem(activeEntries, indexType){
+        const entry = activeEntries[indexType === 'last' ? activeEntries.length - 1 : 0];
+        
+        const freeFinish = entry.querySelector('div.type_free');
+        if (freeFinish){
+            freeFinish.querySelector('div.caption').click();
+        }else{
+            if (indexType === 'last'){
+                const timer = entry.querySelector('span.countdown');
+                if (timer){
+                    addBuildingIntoAutomationQueue(timer.textContent);
+                }else{
+                    console.log('no timer element')
+                }
+            }else{
+                console.log('Item was not ready to finish, something went wrong.')
+            }
+        }
+    }
+
+    function getQueueEntries(){
+        const queue = document.querySelector('.ui_various_orders');
+        if (queue){
+            if (!queue.classList.contains('empty_queue')){
+                const activeEntries = [...queue.children].filter(div => !div.classList.contains('empty_slot'));
+                if (activeEntries.length > 0){
+                    return activeEntries;
+                }
+            }
         }
     }
 
@@ -74,35 +102,48 @@
         if (classes.contains('button_build') || classes.contains('build_button')){
             if (!(classes.contains('build_grey') || classes.contains('disabled'))){
                 await awaitLoading();
-                const queue = document.querySelector('.ui_various_orders');
-                if (queue){
-                    if (!queue.classList.contains('empty_queue')){
-                        const activeEntries = [...queue.children].filter(div => !div.classList.contains('empty_slot'));
-                        if (activeEntries.length > 0){
-                            const newEntry = activeEntries[activeEntries.length - 1];
-                            const freeFinish = newEntry.querySelector('div.type_free');
-                            console.log('freeFinish', freeFinish);
-                            //Free Finish is available
-                            if (freeFinish){
-                                freeFinish.querySelector('div.caption').click();
-                                console.log('clicked on free finish');
-                            //Free Finish is not available
-                            }else{
-                                const timer = newEntry.querySelector('span.countdown');
-                                if (timer){
-                                    addBuildingIntoAutomationQueue(timer.textContent);
-                                }else{
-                                    console.log('no timer element')
-                                }
-                            }
-                        }
-                    }
-                }
+                checkQueueItem(getQueueEntries(), 'last');
             }
         }
     }
 
+    async function goToCity(cityName){
+        let profile = document.querySelector('li[data-option-id="profile"]');
+        if (profile) {
+            profile.click();
+            await awaitLoading();
+            const profileWindowTitleSpan = Array.from(document.querySelectorAll('span')).find(element => element.textContent.trim() === 'Spielerprofil');
+            const profileWindow = profileWindowTitleSpan.parentElement?.parentElement;
+            Array.from(profileWindow.querySelectorAll('a')).find(a => a.textContent.trim() === cityName).click();
+            await awaitLoading();
+            document.querySelector('#goToTown.context_icon').click();
+            await awaitLoading();
+            document.querySelector('button[class="icon_right icon_type_speed ui-dialog-titlebar-close"]').click();
+            await awaitLoading();
+        }
+    }
+    
+    function timerCheck() {
+        const checkTimers = () => {
+            let allTimers = JSON.parse(localStorage.getItem('buildingFinishTimer'));
+            if (!allTimers || allTimers.length === 0) return;
+            allTimers.forEach((buildingEntry) => {
+                const timestamp = new Date(buildingEntry.timestamp);
+                if (timestamp <= new Date()){
+                    goToCity(buildingEntry.cityName);
+                    checkQueueItem(getQueueEntries(), 'first');
+                    allTimers = allTimers.filter(entry => entry !== buildingEntry);
+                }
+            });
+            localStorage.setItem('buildingFinishTimer', JSON.stringify(allTimers));
+        };
+        setInterval(checkTimers, 10000);
+    }
+
     document.addEventListener('click', (event) => {
         clickHandler(event);
-    })
+    });
+
+    timerCheck();
+
 })();
